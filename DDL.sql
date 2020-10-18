09e81699f256173de19fd7b28aab9ef4081bc112
Aicha ABDEL ALI <67974059+aichaabdelali@users.noreply.github.com>
2020-10-18 19:02:43 +0200

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -3,7 +3,7 @@ CREATE TABLE Product(
     product_name VARCHAR2(20) CONSTRAINT cons_product NOT NULL,
     Price NUMBER CONSTRAINT check_price CHECK (Price>0),
 );
-DESC Product;
+DESCRIBE Product;
 
 CREATE TABLE Orders(
     Customer_id VARCHAR2(20) CONSTRAINT pk_customer PRIMARY KEY,
@@ -13,7 +13,7 @@ CREATE TABLE Orders(
     CONSTRAINT fk_customer FOREIGN KEY (Customer_id) REFERENCES Customers (Customer_id),
     CONSTRAINT fk_product FOREIGN KEY (Product_id) REFERENCES Product (Product_id)
 );
-DESC Orders;
+DESCRIBE Orders;
 
 CREATE TABLE Customers(
     Customer_id VARCHAR2(20),
@@ -21,7 +21,7 @@ CREATE TABLE Customers(
     Customer_Tel NUMBER,
     
 );
-DESC Customers;
+DESCCRIBE Customers;
 
 Alter TABLE Product ADD Category VARCHAR2(20);
 Alter TABLE Orders ADD OrderDate DATE(SYSDATE);
